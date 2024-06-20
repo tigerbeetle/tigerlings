cc0b8462bba6d476958eabdad3f258dc75a4c691
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-06-20 09:30:50 -0400

    docs: mention fixed schema of accounts and transfers

--- exercises/003_create_account.sh
+++ exercises/003_create_account.sh
@@ -2,6 +2,12 @@
 
 # Now that we have our TigerBeetle cluster running, we can start sending requests.
 
+# TigerBeetle is a financial transactions database. Unlike a general purpose database,
+# it has a fixed schema of Accounts and Transfers. Accounts track balances and Transfers
+# move value between Accounts.
+
+# Let's start by creating an Account.
+
 # To connect to TigerBeetle, we normally use one of the client libraries.
 # However, for these exercises we're just going to use the REPL command via the CLI.
 
