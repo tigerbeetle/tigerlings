f6e7ea430adbffe83ce9425739d75da3f5bb58d9
Tobias Ziegler <tobias@tigerbeetle.com>
2026-01-26 14:54:07 +0100

    exercise 24-26: fix `post_pending_transfer` calls for TB >= 0.16.0.

--- exercises/026_two_phase_transfers_partial.sh
+++ exercises/026_two_phase_transfers_partial.sh
@@ -14,12 +14,12 @@ tb "create_transfers id=26000 debit_account_id=2600 credit_account_id=2601 amoun
                      id=26002 debit_account_id=2600 credit_account_id=2601 amount=100 ledger=260 code=10 flags=pending;"
 
 # Now, we'll post them -- but we're going to specify different amounts:
-output=$(tb "create_transfers id=26003 pending_id=26000 amount=0    flags=post_pending_transfer,
-                              id=26004 pending_id=26001 amount=200  flags=post_pending_transfer,
-                              id=26005 pending_id=26002 amount=200  flags=post_pending_transfer;")
+output=$(tb "create_transfers id=26003 pending_id=26000 amount=500 flags=post_pending_transfer,
+                              id=26004 pending_id=26001 amount=200 flags=post_pending_transfer,
+                              id=26005 pending_id=26002 amount=200 flags=post_pending_transfer;")
 
 # Uh oh! That didn't work!
-# The first transfer is actually okay -- specifying an amount of 0 means the full amount is posted.
+# The first transfer is actually okay -- specifying the same amount as the pending transfer posts the full amount.
 # The second is also okay, because the amount is less than the pending transfer.
 # But that third transfer...
 # Can you fix it?
